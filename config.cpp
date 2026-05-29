9639857b6777e4299463a3b5ac84335dcfe79bf5
Hacket2130 <loganmatthewgreaves@gmail.com>
2026-05-29 10:35:41 +0100

    v 0.1.1
    Tried fixing ambience.hpp not found - fixed incorrect slash designations

--- config.cpp
+++ config.cpp
@@ -14,18 +14,18 @@ class CfgSounds
 {
     sounds[] = {};
 
-    #include "ambience/ambience.hpp"
-    #include "cis/cis.hpp"
-    #include "gar/gar.hpp"
-    #include "galactic_Senate/galactic_senate.hpp"
-    #include "misc_Factions/misc_factions.hpp"
-    #include "mission_Openings/mission_openings.hpp"
+    #include "ambience\ambience.hpp"
+    #include "cis\cis.hpp"
+    #include "gar\gar.hpp"
+    #include "galactic_Senate\galactic_senate.hpp"
+    #include "misc_Factions\misc_factions.hpp"
+    #include "mission_Openings\mission_openings.hpp"
 };
 
 
-// CFG MUSIC: Background tracks and Zeus/Eden Music Modules
+// CFG MUSIC: Background tracks and Zeus\Eden Music Modules
 
 class CfgMusic
 {
-    #include "music/music.hpp"
+    #include "music\music.hpp"
 };
--- tcwa3_audio_mod/ambience/ambience.hpp
+++ tcwa3_audio_mod/ambience/ambience.hpp
@@ -2,7 +2,7 @@
 class rep_distant_battle
 {
     name = "[TCW Ambience] Distant Battle (Republic)";
-    sound[] = {"/tcwa3_audio_mod/Ambience/Battle/rep_distant_battle.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\Battle\rep_distant_battle.ogg", db+0, 1.0};
     titles[] = {};
 };
 
@@ -10,13 +10,13 @@ class rep_distant_battle
 class bunker_amb1
 {
     name = "[TCW Ambience] Bunker (Clean)";
-    sound[] = {"/tcwa3_audio_mod/ambience/buildings/bunker/clean/bunker_amb1.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\buildings\bunker\clean\bunker_amb1.ogg", db+0, 1.0};
     titles[] = {};
 };
 class bunker_sep1
 {
     name = "[TCW Ambience] Bunker (Separatist)";
-    sound[] = {"/tcwa3_audio_mod/ambience/buildings/bunker/seperatist/bunker_sep1.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\buildings\bunker\seperatist\bunker_sep1.ogg", db+0, 1.0};
     titles[] = {};
 };
 
@@ -24,13 +24,13 @@ class bunker_sep1
 class factory_amb1
 {
     name = "[TCW Ambience] Factory 1";
-    sound[] = {"/tcwa3_audio_mod/ambience/buildings/factory/factory_amb1.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\buildings\factory\factory_amb1.ogg", db+0, 1.0};
     titles[] = {};
 };
 class factory_saylar1
 {
     name = "[TCW Ambience] Factory Saylar 1";
-    sound[] = {"/tcwa3_audio_mod/ambience/buildings/factory/factory_saylar1.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\buildings\factory\factory_saylar1.ogg", db+0, 1.0};
     titles[] = {};
 };
 
@@ -38,18 +38,18 @@ class factory_saylar1
 class Dathomir_forest
 {
     name = "[TCW Ambience] Dathomir Forest";
-    sound[] = {"/tcwa3_audio_mod/Ambience/Dathomir/Dathomir/forest.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\Ambience\Dathomir\Dathomir\forest.ogg", db+0, 1.0};
     titles[] = {};
 };
 class Dathomir_mountain
 {
     name = "[TCW Ambience] Dathomir Mountain";
-    sound[] = {"/tcwa3_audio_mod/ambience/dathomir/dathomir/mountain.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\dathomir\dathomir\mountain.ogg", db+0, 1.0};
     titles[] = {};
 };
 class Dathomir_swamp
 {
     name = "[TCW Ambience] Dathomir Swamp";
-    sound[] = {"/tcwa3_audio_mod/ambience/dathomir/dathomir/swamp.ogg", db+0, 1.0};
+    sound[] = {"\tcwa3_audio_mod\ambience\dathomir\dathomir\swamp.ogg", db+0, 1.0};
     titles[] = {};
 };
